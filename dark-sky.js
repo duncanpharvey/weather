3d3401f5cfdeacb2dd5c172f4aad0fb371360f94
Duncan Harvey <duncanpharvey@gmail.com>
2020-10-17 19:26:19 -0400

    adds dark sky api call

--- dark-sky.js
+++ dark-sky.js
@@ -2,7 +2,16 @@ const DarkSky = require('dark-sky');
 const darkSky = new DarkSky(process.env.DARK_SKY);
 
 async function forecast() {
-    console.log('forecast function call');
+    await darkSky
+        .options({
+            exclude: ['minutely', 'hourly', 'daily', 'flags'],
+            language: 'en',
+            latitude: 37.8267,
+            longitude: -122.423,
+            units: 'si'
+        })
+        .get()
+        .then(res => console.log(res));
 }
 
 module.exports = {
